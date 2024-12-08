66aaa6ea6c12d7573df3efc35a3980b66d533335
Ankit Suman <ankitsuman07@gmail.com>
2024-12-08 20:30:47 +0530

    Implement pagination

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,41 +1,49 @@
-import React, {useContext} from 'react'
-import SearchFilter from '../../components/search-filter/SearchFilter'
+import React, { useContext } from 'react';
+import SearchFilter from '../../components/search-filter/SearchFilter';
 
-import MainImage from "../../assets/main-pic.png"
-import "./Home.css"
+import MainImage from '../../assets/main-pic.png';
+import './Home.css';
 
-import NavComponent from '../../components/nav-bar/NavComponent'
-import BlogPostComp from '../../components/blog-post/BlogPostComp'
+import NavComponent from '../../components/nav-bar/NavComponent';
+import BlogPostComp from '../../components/blog-post/BlogPostComp';
 
-import { BlogContext } from '../../context/BlogContext'
+import { BlogContext } from '../../context/BlogContext';
 
 const Home = () => {
-  const { data } = useContext(BlogContext);
+  const { data, limit, setLimit } = useContext(BlogContext);
+
+  const handlePagination = () => {
+    setLimit(limit+10)
+  }
 
   return (
-    <div className='main-area'>
-      <nav>
+    <div className="main-area">
       <NavComponent />
-      </nav>
-      
-        <main>
-            <section className='hero' >
-              <img src={MainImage} alt='main-hoarding' />
-            </section>
-            <section className='search-section' >
-              <SearchFilter />
-            </section>
-            <section className='blogs' >
-              {data && data.map((item) => (
-                <BlogPostComp key={item.id}  blog={item} />
-              ))}
-            </section>
-        </main>
-        <footer>
 
-        </footer>
+      <main>
+        <section className="hero">
+          <img src={MainImage} alt="Main hoarding showcasing blog content" />
+        </section>
+        <section className="search-section">
+          <SearchFilter />
+        </section>
+        <section className="blogs">
+          {data && data.length > 0 ? (
+            data.map((item) => <BlogPostComp key={item.id} blog={item} />)
+          ) : (
+            <p>No blogs available</p>
+          )}
+        </section>
+        <section className="pagination" onClick={handlePagination} >
+          More <span>▼</span>
+        </section>
+      </main>
+
+      <footer>
+        <p>© 2024 BlogSite. All rights reserved.</p>
+      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
